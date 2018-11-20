58566f609bb7a4c67ebe433e1083b9038120e305
Sergey <relzet69@gmail.com>
2018-11-20 22:23:31 +0200

    added "Collections: Map of Strings, Strings" domain example

---
+++ src/main/java/com/borovyksv/model/collections/mapofstrings/MosItem.java
@@ -0,0 +1,56 @@
+package com.borovyksv.model.collections.mapofstrings;
+
+import com.borovyksv.model.Constants;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+
+@Entity
+public class MosItem {
+
+    @Id
+    @GeneratedValue(generator = Constants.ID_GENERATOR)
+    private Long id;
+    private String itemName;
+
+    @ElementCollection
+    @CollectionTable(name = "Image",                          //overrides table "SosItem_images" -> "Image",
+            joinColumns = @JoinColumn(name = "item_id"))      //overrides column "SosItem_id" -> "item_id",
+    @MapKeyColumn(name = "filename")                          //key
+    @Column(name = "imagename")                               //value
+    protected Map<String, String> images = new HashMap<>();
+}
+
+/**
+ * EXAMPLE:
+ *
+ * create table Image (
+ *        item_id bigint not null,
+ *         imagename varchar(255),
+ *         filename varchar(255) not null,
+ *         primary key (item_id, filename)
+ *     ) engine=InnoDB
+ * Hibernate:
+ *
+ *     create table MosItem (
+ *        id bigint not null,
+ *         item_name varchar(255),
+ *         primary key (id)
+ *     ) engine=InnoDB
+ * Hibernate:
+ *
+ *     alter table Image
+ *        add constraint FKdi59i5qggttd5gm1k2d4ip723
+ *        foreign key (item_id)
+ *        references MosItem (id)
+ * */
